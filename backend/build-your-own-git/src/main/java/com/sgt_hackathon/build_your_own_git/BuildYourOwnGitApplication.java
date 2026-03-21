8ca80ef60a7c1d1eb51b68b73b407fd22fd3a580
Dolly Mangwani <mangwanidolly777@gmail.com>
2026-03-21 12:50:58 +0530

    Initial commit: Added Angular frontend and Spring Boot backend

---
+++ backend/build-your-own-git/src/main/java/com/sgt_hackathon/build_your_own_git/BuildYourOwnGitApplication.java
@@ -0,0 +1,13 @@
+package com.sgt_hackathon.build_your_own_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildYourOwnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildYourOwnGitApplication.class, args);
+	}
+
+}
---
+++ backend/build-your-own-git/src/test/java/com/sgt_hackathon/build_your_own_git/BuildYourOwnGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgt_hackathon.build_your_own_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildYourOwnGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
